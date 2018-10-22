f02df865e145bbc53cb07d6e4582c626d6a19d7b
Gabriel Back <42013655+GabrielBack28@users.noreply.github.com>
2018-10-22 18:57:17 -0300

    Update TestCalcpy.py
    Corrigir nome da classe

--- TestCalcpy.py
+++ TestCalcpy.py
@@ -1,7 +1,7 @@
 import unittest
 from CALCPY import *
 
-class TescCalcpy(unittest.TestCase):
+class TestCalcpy(unittest.TestCase):
     def test_format_input_numeric(self):
         self.assertEqual(format_input_numeric("3"), 3.0)
         self.assertEqual(format_input_numeric("3.1"), 3.1)
