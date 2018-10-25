9c1e640b19d979d80810f19a6d5b7307057518b2
Gabriel Back <42013655+GabrielBack28@users.noreply.github.com>
2018-10-24 21:04:11 -0300

    Update TestCalcpy.py
    Implementando teste para validar valor maior que 0;
    Implementando teste para validação de entrada "sim" ("is_yes", "Y").

--- TestCalcpy.py
+++ TestCalcpy.py
@@ -6,3 +6,16 @@ class TestCalcpy(unittest.TestCase):
         self.assertEqual(format_input_numeric("3"), 3.0)
         self.assertEqual(format_input_numeric("3.1"), 3.1)
         self.assertEqual(format_input_numeric("4,2"), 4.2)
+        
+    def test_is_value_valid(self):
+        self.assertEqual(is_value_valid("0"), False)
+        self.assertEqual(is_value_valid("1"), True)
+        self.assertEqual(is_value_valid("250"), True)
+        self.assertEqual(is_value_valid("-1"), False)
+        
+    def test_is_yes(self):
+        self.assertEqual(is_yes("y"), True)
+        self.assertEqual(is_yes("Y"), True)
+        self.assertEqual(is_yes("o"), False)
+        self.assertEqual(is_yes("F"), False)
+        self.assertEqual(is_yes("N"), False)
